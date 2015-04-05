f01470d5004bcb7e663999418e35d124aceda5ce
Jayanti Dabhere & Amol Patil <amolpatil.ap@gmail.com>
2015-04-05 12:05:57 -0700

    COLOR-1 Add root path and render static view

---
+++ spec/requests/art_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "Art Controller", type: :request do
+	it "renders homepage successfully" do
+    get "/"
+    expect(response).to have_http_status(200)
+    expect(response.body).to include("EXPLORE OTHERS ARTSY CREATIONS OR SHARE YOURS...BECAUSE THERE IS NOTHING MORE FUN THAN DIY!")
+	end
+end	
